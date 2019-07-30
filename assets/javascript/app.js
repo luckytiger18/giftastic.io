baa1c337d6aaafc6d6a04674739c8649c15a9853
Annie Luc <lannie3186@gmail.com>
2019-07-29 20:29:23 -0700

    added addButton, onclick, and clear function.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,37 @@
-//javascript, jQuery
+var topics = ["Coffee", "Tea", "Ice Cream", "Doughnut", "Bacon", "Cookie", "Taco", "Cake", "Beer"]
+
+//clear function
+function clear() {
+    $("#foodButtons").empty()
+}
+
+//console.log(topics.length);
+// for loop through each of the array elements
+function addButton() {
+    clear();
+    for (var i = 0; i < topics.length; i++) {
+
+        var button = $("<button>").text(topics[i]);
+        //appending the button to the top
+        $("#foodButtons").append(button)
+    }
+}
+$("#addFood").on("click", function () {
+    var value = $("#foodInput").val();
+    topics.push(value);
+    console.log(topics);
+    addButton();
+    event.preventDefault();
+})
+//everytime I click on the button, console.log of what the the text of the button is. 
+
+
+//execution, javascript, jQuery, api address
 console.log("hello world!")
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=JB57LXX6RTn0Td7BIpM5YCGLbYf7Z0Ls&limit=5");
-xhr.done(function(data) { console.log("success got data", data); });
+var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=coffee&api_key=JB57LXX6RTn0Td7BIpM5YCGLbYf7Z0Ls&limit=10");
+xhr.done(function (data) { console.log("success got data", data); });
+
+addButton()
+
+
+    // var URL = "https://api.giphy.com/v1/gifs/search?q=" + topics "&api_key=JB57LXX6RTn0Td7BIpM5YCGLbYf7Z0Ls&limit=10"
